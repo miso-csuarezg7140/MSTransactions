0fe94424e616e6574d661b561733b9641ecb3cdb
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2024-06-18 22:18:52 -0500

    Initial commit

---
+++ src/main/java/com/novatec/MSTransactions/MsTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.novatec.MSTransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novatec/MSTransactions/MsTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novatec.MSTransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
